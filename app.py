7f6acf4db997b43498839de10f8e08bdc6920cd0
yamato0331 <yamato020331@gmail.com>
2024-11-03 14:11:48 +0900

    Remove spacy and use textblob

--- app.py
+++ app.py
@@ -21,25 +21,5 @@ def get_bot_response():
     else:
         return str(chatbot.get_response(user_text))
 
-# ファイルのアップロードを処理
-@app.route("/upload", methods=["POST"])
-def upload_file():
-    if 'file' not in request.files:
-        return "ファイルが選択されていません", 400
-    file = request.files['file']
-    if file.filename == '':
-        return "ファイル名が無効です", 400
-
-    # アップロードファイルの拡張子を取得
-    file_extension = os.path.splitext(file.filename)[1].lower()
-
-    # アップロード可能なファイルの種類を制限
-    if file_extension not in ['.pdf', '.doc', '.docx']:
-        return "PDFまたはWord形式のファイルをアップロードしてください", 400
-
-    # ファイルを保存する
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-    return "退職届をアップロードしました。不備がないか確認しますのでお待ちください", 200
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=10000, debug=True)
